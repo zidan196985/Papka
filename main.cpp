5ab2eb5f2d79bf97ed5c121d25320fcabd7f1a96
zidan196985 <z.baxtiyorov@newuu.uz>
2024-09-13 12:24:50 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    //Problem1
+//    int intNumber=22;
+//    float floatNumber=3.14;
+//    double doubleNumber=45.1234;
+//    bool boolean=true;
+//    char charName='A';
+//    cout << "Value of Integer is " <<intNumber<<" Size is "<<sizeof(intNumber)<< endl<<"Value of Float is "<<floatNumber<<" Size is "<<sizeof(floatNumber)<<endl<<"Value of Double is "<<doubleNumber<<" Size is "<<sizeof(doubleNumber)<<endl<<"Value of Char is"<<charName<<" Size is "<<sizeof(charName)<<endl<<"Value of Bool is "<<boolean<<" Size is "<<sizeof(boolean);
+//    return 0;
+    //Problem2
+//    cout<<"Abidov Davron\n"<<"240471\n"<<"Day light\n"<<"Avengers";
+
+    //Problem3
+//    cout<<"\"I’m standing\" on the edge of some crazy cliff.\n";
+//    cout<<"What I have to do, I have to catch everybody\\\n";
+//    cout<<"if they start to go over the cliff—I mean\\\n";
+//    cout<<"\\if they\’re running and they don\’t look\n";
+//    cout<<"where they\’re going I have to come out\n";
+//    cout<<"from somewhere and catch them.\n";
+    //Problem4
+//   int a=189;
+//   char b='B';
+//   int decimal_value_b=b;
+//
+//   cout<<a/4.0<<" "<<decimal_value_b<<" "<<(float)(a+decimal_value_b)/5<<" "<<(char) ((a/3-7)+decimal_value_b);
+    //Problem5
+//    int a=12;
+//    int b=38;
+//cout<<"Sum= "<<a+b<<endl<<"Product= "<<a*b;
+//
+//Problem 6
+
+
+//int a;
+//int b;
+//cout<<"Enter first number!!"<<endl;
+//cin>>a;
+//cout<<"Enter second numbers!!"<<endl;
+//    cin>>b;
+//
+//cout<<"sum is "<<a+b<<endl;
+
+//Problem 7
+
+//float x;
+//float y;
+//cout<<"output x and y:"<<endl;
+//cin>>x>>y;
+//
+//cout<<"t is equal to "<<((3*(x*x+3))/((y/4)+3));
+//
+//Problem 8
+
+//cout<<"Welcome to Area calculator, Dont forget to buy subcristion and enter the DIAMETR"<<endl;
+//float a;
+//cin>>a;
+//cout<<"You are welcome="<<(4*pow((a/2),2)*3.14)<<endl;
+//
+//Problem 9
+//
+//
+//float v1;
+//float v0;
+//float t;
+//
+//cout<<"Enter Starting velocity!";
+//cin>>v0;
+//    cout<<"Enter Ending velocity!";
+//cin>>v1;
+//    cout<<"Enter time Span!";
+//    cin>>t;
+//
+//cout<<"The average Acceleration is "<<((v1-v0)/t)<<endl;
+//
+////
+//Problem 10
+//
+//float s;
+//float f;
+//float p;
+//
+//cout<<"Input Distance ";
+//cin>>s;
+//    cout<<"Input Fuel efficiency ";
+//    cin>>f;
+//
+//    cout<<"Input Price per galloon ";
+//    cin>>p;
+//
+//cout<<"It'll be roughly "<<((s/f)*p);
+//
+
+////Problem 111
+//
+//double a=1.533e4;
+//double b=424e-3;
+//
+//cout<<a<<endl<<b;
+//
+
+//Problem 12
+
+//int a;
+//cin>>a;
+//int b;
+//int c;
+//b=a/11;
+//c=a%11;
+//cout<<"First group ="<<b<<"\nSecond= "<<c;
+//Problem 14
+
+//char a='A';
+//int cc=a;
+//
+//int h;
+//int e;
+//int l;
+//int o;
+
+//h=cc+7;
+//e=cc+4;
+//l=cc+11;
+//o=cc+14;
+//
+//cout<<(char) h<<(char)e<<(char)l<<(char)l<<(char)o;
+//Problem 15
+
+
+
+
+}
